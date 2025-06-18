bc4936cdea31c2ba95fb41fda51f49a3cc370328
GYAN224143 <gyan224143@gmail.com>
2025-06-19 00:03:37 +0530

    updated url

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const ChatMessage = mongoose.model("ChatMessage", chatMessageSchema);
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatbot-adwance.netlify.app",
     credentials: true,
   })
 );
